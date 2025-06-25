fc88d3a002a889762ba992426ffb058a2a223964
aanujkothari <96535694+aanujkothari@users.noreply.github.com>
2025-06-25 18:19:58 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 308
+let accountEmail = 'shelya@ak.com'
+let accountCity = 'Oslo'
+accountState = 'WA'
+let accountCountry;
+
+// accountID = 885 Assignment to constant variable not allowed.
+console.table([accountID,accountEmail,accountCity,accountState,accountCountry]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+let age = undefined
+console.log(typeof age);
+
+let temp = null
+console.log(typeof temp);
